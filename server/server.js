ce37a8e39f93e2cefd2a828f71cc8508e6495a01
Samuel <samuelou510@gmail.com>
2023-12-03 20:20:34 -0500

    Revert "Revert "env edit""
    This reverts commit cace85867e7a950ca24cdfb5cc94429db3652c13.

--- server/server.js
+++ server/server.js
@@ -7,19 +7,6 @@ mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection
 db.on('error', (error)=> console.error(error))
 db.once('open', ()=>console.log('connected to db'))
-// const uri =
-//   "mongodb+srv://samuelou510:Ll9u8509LsGrvNvB@cluster0.w0qyptv.mongodb.net/?retryWrites=true&w=majority";
-// //const uri
-// app.use(express.json());
-// async function connect() {
-//   try {
-//     await mongoose.connect(uri);
-//     console.log("mongo connected");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// connect();
 
 const usersRouter = require('./routes/users')
 app.use('/users', usersRouter)
@@ -27,10 +14,6 @@ app.use('/users', usersRouter)
 
 app.use(express.json())
 app.use("/", router);
-
-
-
-
 
 
 const port = process.env.PORT || 8000;
