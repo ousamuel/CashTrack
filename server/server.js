0fb532c296a67c0cf475e063044490e5dfe18173
Samuel <samuelou510@gmail.com>
2024-02-08 22:32:07 -0500

    options

--- server/server.js
+++ server/server.js
@@ -78,11 +78,7 @@ if (process.env.NODE_ENV === "production") {
   sess.cookie.sameSite = "none";
 }
 app.use(sessions(sess));
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
+
 
 // app.use(
 //   cors({
